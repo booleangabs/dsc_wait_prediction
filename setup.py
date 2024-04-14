c6a02ea84e96bba7c7777da30c45247a94dd699d
booleangabs <gabrieltavares1303@gmail.com>
2024-04-14 05:09:33 -0300

    chore: Added environment configuration/dependencies registry files

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import find_packages, setup
+import os
+import io
+import re
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding="utf-8") as fd:
+        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
 
 setup(
-    name='src',
+    name='dsc_wait_prediction',
     packages=find_packages(),
-    version='0.1.0',
+    version="0.0.1",
+    url="https://github.com/booleangabs/dsc_wait_prediction",
+    author="José Gabriel Pereira Tavares",
+    author_email="gabrieltavares1303@gmail.com",
+    long_description=read("README.md"),
     description='Submission to Data Science Challenge @ EEF promoted by ITA - Brazil',
-    author='José Gabriel Pereira Tavares',
     license='MIT',
 )
